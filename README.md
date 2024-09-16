345af2fd6b4b6e6bf1f2541ab02389c09351704c
sandra444 <sandrakarcher44@gmail.com>
2024-09-16 12:50:07 -0400

    added some notes to the setup.py file and git ignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ncar_python_tutorial
 Working through the tutorial from Pythia/NCAR to get a better understanding of this content.
+
+A few notes:
+-This tutorial is from here https://ncar.github.io/python-tutorial/tutorials/yourfirst.html#using-a-built-in-package
+-Some notes are in the files themselves.
+-Some notes are in my computer setup file (personal Word document ComputerSetup_Dellp7560Comprehensive2OnGoing.docx).
+-Some notes are here.
+
+
--- setup.py
+++ setup.py
@@ -24,3 +24,23 @@ setup(
 )
 
 # dependencies (install_requires) which is set to an empty list since our current package uses no external packages.
+# install package (test to local computer)
+# $ pip install .
+# to confirm, use
+# $ conda list    (we should see mysci)
+# to uninstall
+# $ pip uninstall mysci
+# install from GitHub
+# $ pip install git+https://github.com/Username/Project.git
+# https://github.com/sandra444/ncar_python_tutorial.git
+# (fileplay) C:\Users\Sandra Karcher\OneDrive\adell\repos\ncar_python_tutorial>pip install git+https://github.com/sandra444/ncar_python_tutorial.git
+# $ conda list
+# to put on PyPI
+# $ pip install twine
+# By installing this package, a new utility called twine will be installed that you can use to upload your package. 
+# First, however, we need to build a distribution package using our newly created setup.py file. 
+# To do that, execute the following command in the same directory where the setup.py file is located:
+# $ python setup.py sdist bdist_wheel
+# Then, to upload your newly created distribution package to PyPI, execute the following:
+# $ twine upload dist/*
+# see this page for more details https://ncar.github.io/python-tutorial/tutorials/yourfirst.html#using-a-built-in-package
