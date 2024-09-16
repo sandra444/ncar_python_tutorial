1cd60aee8b9fbff86717e7ff0577a988a45eb334
sandra444 <sandrakarcher44@gmail.com>
2024-09-16 12:22:16 -0400

    adding a setup.py file for the tutorial

---
+++ setup.py
@@ -0,0 +1,24 @@
+# https://ncar.github.io/python-tutorial/tutorials/yourfirst.html#using-a-built-in-package
+# reminders
+# in outer directory
+# $ git clone https://github.com/username/some_package.git
+# $ pip install some_package
+# or, shortcut two in one
+# $ pip install git+https://github.com/username/some_package.git
+# if the package is on PyPI, it is even easier
+# $ pip install some_package
+
+# The setup.py file is a Python file necessary for package distribution. 
+# This file tells pip how to install your package into the common Python space for your python interpreter. 
+
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Xdev",
+    author_email="xdev@ucar.edu",
+    packages=["mysci"],
+    install_requires=[],
+)
